109947d2bb083830d52345b2c4f63826a460d465
Jade Wei <77248838+jadew33@users.noreply.github.com>
2023-12-11 05:35:25 -0500

    test

--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -5,14 +5,16 @@ export default function useKeyPress(targets) {
   function downHandler({ key }) {
     targets.map((target) => {
       if (key === target) setKeyPressed(true);
+      return null;
     });
   }
   const upHandler = ({ key }) => {
     targets.map((target) => {
       if (key === target) setKeyPressed(false);
+      return null;
     });
   };
-
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     window.addEventListener("keydown", downHandler);
     window.addEventListener("keyup", upHandler);
@@ -20,6 +22,7 @@ export default function useKeyPress(targets) {
       window.removeEventListener("keydown", downHandler);
       window.removeEventListener("keyup", upHandler);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []); // Empty array ensures that effect is only run on mount and unmount
   return keyPressed;
 }
